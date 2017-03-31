013b5273b51bc6a7e7dd543a25672b9f561728c6
tfushitani <gfushitani@gmail.com>
2017-03-31 20:04:25 +0900

    APIを実行するだけのテストアプリ

---
+++ work/WebApiCommander/WebApiCommander/WebApiCommander/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+
+namespace WebApiCommander
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        async void Button_ClickedAsync(object sender, System.EventArgs e)
+        {
+            // ログ出力する
+            System.Diagnostics.Debug.WriteLine("Button Clicked");
+
+            //非同期でダウンロード
+            var str = await Download(entry1.Text);
+
+            // ラベルに表示する
+            label1.Text = str;
+
+        }
+
+        async Task<String> Download(String url)
+        {
+            var httpClient = new HttpClient();
+            return await httpClient.GetStringAsync(url);
+        }
+    }
+}
---
+++ work/WebApiTest/WebApiTest/WebApiTest.Android/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace WebApiTest.Droid
+{
+	[Activity (Label = "WebApiTest", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+            // レイアウトを設定する
+            SetContentView(Resource.Layout.layout1);
+
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+            var button = FindViewById<Button>(Resource.Id.button1);
+            var edittext = FindViewById<TextView>(Resource.Id.textView1);   
+
+            button.Click += (s, e) =>
+            {
+
+            };
+
+
+		}
+	}
+}
+
